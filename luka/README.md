7deedceb1f2b339070b1752122547fd8ff661a4a
Luka Nikabadze <niqabadzeluka@gmail.com>
2019-12-10 11:12:51 -0500

    PerfectNumbers, Factorors

---
+++ luka/README.md
@@ -0,0 +1,28 @@
+This is multithreaded java program which calculates if number is PerfectNumber or not. If the number is PerfectNumber program also calculates its factors.
+To compile program use command javac PerfectNumber.java
+to run program use command  java PerfectNumber
+
+example outputs:
+
+Enter your number
+28
+Enter the number of partitions
+3
+28 is a PerfectNumber and its factors are 1, 2, 4, 7, 14
+
+
+Enter your number
+30
+Enter the number of partitions
+4
+it is not a PerfectNumber (1)
+
+
+Enter your number
+496
+Enter the number of partitions
+4
+496 is a PerfectNumber and its factors are 1, 2, 4, 8, 16, 31, 62, 124, 248
+
+
+Thank you.
--- luka/perfectnumber.java
+++ luka/perfectnumber.java
@@ -1,94 +1,107 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
+
 /*
 * PerfectNumber class implements runnable
 */
-public class PerfectNumber implements Runnable{
-   private static int sum=1, index;
-   private static int[] factorials ;
-   private static int num;
-   private int [] imputNums;
-   private static int partitionNo=0;
+public class PerfectNumber implements Runnable {
 
-   public PerfectNumber(int [] imputNums){
-       this.imputNums = imputNums;
-   }
+private static int sumOfFactorials = 1, index;
+private static int[] factorials;
+private static int number;
+private int[] imputNumbers;
+private static int partitionNo = 0;
+private static ArrayList<Integer> factors = new ArrayList<>();
 
-   public static void main(String[] args) {
-       int p;
+  public PerfectNumber(int[] imputNumbers) {
+   this.imputNumbers = imputNumbers;
+  }
 
-       System.out.println("Enter a number");
+  public static void main(String[] args) {
+  int p;
 
-       Scanner in = new Scanner(System.in);
-       num = in.nextInt();
-       System.out.println("Enter number of partitions ");
-       p = in.nextInt();
-       int result = isPerfectNumber(p);
-       if(result==0){
-           System.out.println("Its a perfect number");
-       }else{
-           System.out.println("Its not a perfect number");
-       }
+  System.out.println("Enter your number");
 
-   }
-   /*
-   * method that creates p threads and calculates if perfect number or not
-   */
-   public static int isPerfectNumber( int p){
-       if (num > 0 && p<num) {
-           factorials = new int[num];
-           int noInEachPartition = num / p;
-           int remain = num%p;
-           if(partitionNo == p)
-               noInEachPartition = noInEachPartition + remain;
-           int[] imputNums = new int[noInEachPartition];
-           int i = 1;
-           while(i<num){
-               for (int j = 0; j < noInEachPartition && i<num ; j++) {
-                   imputNums[j] = i;
-                   //System.out.print("p"+i);
-                   i++;
-               }
-               partitionNo++;
-
-               Thread object = new Thread(new PerfectNumber(imputNums));
-   object.start();
-   try {
-                   object.join();
-               } catch (InterruptedException e) {
-                   // TODO Auto-generated catch block
-                   e.printStackTrace();
-               }
-           }
-           if(sum==num ){
-               return 0;
-           }else{
-               return 1;
-           }
-
-       }else return -1;
-
-
-   }
-
-   @Override
-   public void run() {
-       // TODO Auto-generated method stub
-       for (int i = imputNums[imputNums.length-1]; i >imputNums[0] ; i--) {
-
-           /* sum of it's factors */
-               if ( num % i == 0) {
-                   sum = sum + i;
-                   factorials[index]=i;
-                   index = index+1;
-                   //System.out.println(num);
-                   //System.out.println(i);
-               }
-           }
-
-
-
-
-
-   }
-
+  Scanner in = new Scanner(System.in);
+  number = in.nextInt();
+  System.out.println("Enter the number of partitions ");
+  p = in.nextInt();
+  factors.add(1);
+  int result = isPerfectNumber(p);
+  // sort the factors in ascending oorder (optional)
+  Collections.sort(factors);
+  if (result == 0) {
+    System.out.print(number + " is a PerfectNumber and its factors are ");
+      for(int i = 0; i < factors.size(); i++)
+       {
+          if(i == factors.size() - 1)
+          System.out.println(factors.get(i));
+           else
+            System.out.print(factors.get(i) + ", ");
+            }
+          }
+       if (result != 0) {
+     System.out.println("it is not a PerfectNumber (1)");
+  }
 }
+
+/*
+* method that creates p threads and calculates if perfect number or not
+*/
+public static int isPerfectNumber(int p) {
+  if (number > 0 && p < number) {
+  factorials = new int[number];
+  int noInEachPartition = number / p;
+  int remain = number % p;
+  if (partitionNo == p) {
+  noInEachPartition = noInEachPartition + remain;
+ }
+ int[] imputNumbers = new int[noInEachPartition];
+ int i = 1;
+ while (i < number) {
+ for (int j = 0; j < noInEachPartition && i < number; j++) {
+ imputNumbers[j] = i;
+ //System.out.print("p"+i);
+ i++;
+}
+partitionNo++;
+
+Thread object = new Thread(new PerfectNumber(imputNumbers));
+object.start();
+try {
+ object.join();
+   } catch (InterruptedException e) {
+   // TODO Auto-generated catch block
+   e.printStackTrace();
+  }
+ }
+if (sumOfFactorials == number) {
+return 0;
+  }
+    else {
+    return 1;
+    }
+
+  }
+  else {
+  return -1;
+  }
+
+ }
+
+@Override
+public void run() {
+// TODO Auto-generated method stub
+for (int i = imputNumbers[imputNumbers.length - 1]; i > imputNumbers[0]; i--) {
+
+/* sumOfFactorials of it's factors */
+if (number % i == 0) {
+    sumOfFactorials = sumOfFactorials + i;
+    factors.add(i);
+    factorials[index] = i;
+    index = index + 1;
+     }
+    }
+   }
+ }
